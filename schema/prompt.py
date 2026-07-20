932e855a774732a2cca91046e931d5888f8bb702
vNullptr <ghstdu@gmail.com>
2026-07-20 10:31:29 +0100

    add: added doc strings

--- schema/prompt.py
+++ schema/prompt.py
@@ -1,11 +1,21 @@
 from pydantic import BaseModel, Field
 
 class PromptSchema(BaseModel):
+    """Prompt Schema"""
+    
     prompt : str | None = Field()
-    role : str = Field(default="user")
+    role : str = Field(default="human")
 
     @property
-    def structured_prompt(self) -> dict[str, str]:
+    def structured_prompt(self) -> tuple[str, str]:
+        """Structures prompt for langchain invoke().
+
+        Raises:
+            RuntimeError: Raises runtime error if prompt field isn't set.
+
+        Returns:
+            Returns valid format prompt based on langchain conventions. 
+        """
         if self.prompt is None:
             raise RuntimeError("Missing prompt in PromptSchema.")
-        return {"role":self.role, "message":self.prompt} 
+        return (self.role,self.prompt)
