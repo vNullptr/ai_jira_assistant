8ae9831ebd32fd69109989143c687b9b2665ea41
vNullptr <ghstdu@gmail.com>
2026-07-20 13:25:01 +0100

    refactor: removed prompt schema class, now temporarly storing hard coded test prompt

--- schema/prompt.py
+++ schema/prompt.py
@@ -1,21 +1,8 @@
-from pydantic import BaseModel, Field
+from langchain_core.prompts import ChatPromptTemplate
 
-class PromptSchema(BaseModel):
-    """Prompt Schema"""
-    
-    prompt : str | None = Field()
-    role : str = Field(default="human")
 
-    @property
-    def structured_prompt(self) -> tuple[str, str]:
-        """Structures prompt for langchain invoke().
-
-        Raises:
-            RuntimeError: Raises runtime error if prompt field isn't set.
-
-        Returns:
-            Returns valid format prompt based on langchain conventions. 
-        """
-        if self.prompt is None:
-            raise RuntimeError("Missing prompt in PromptSchema.")
-        return (self.role,self.prompt)
+basePrompt = ChatPromptTemplate(
+    [
+        ("human", "test prompt mistral.")
+    ]
+)
