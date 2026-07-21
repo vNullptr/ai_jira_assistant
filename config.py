e2b9bba813c832822e3c0d9798f705e9b43ff8a1
vNullptr <ghstdu@gmail.com>
2026-07-21 13:31:41 +0100

    add: config of langfuse env variables.

--- config.py
+++ config.py
@@ -1,6 +1,20 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
 
 class Settings(BaseSettings):
-    
+
+    # LANGFUSE
+    LANGFUSE_PUBLIC_KEY : str
+    LANGFUSE_SECRET_KEY : str
+    LANGFUSE_BASE_URL : str     
     
     model_config = SettingsConfigDict(env_file=".env")
+    
+    
+    def model_post_init(self, context):
+        if self.LANGFUSE_PUBLIC_KEY and self.LANGFUSE_SECRET_KEY:
+            os.environ["LANGFUSE_SECRET_KEY"] = self.LANGFUSE_SECRET_KEY
+            os.environ["LANGFUSE_PUBLIC_KEY"] = self.LANGFUSE_PUBLIC_KEY
+            os.environ["LANGFUSE_BASE_URL"] = self.LANGFUSE_BASE_URL 
+        
+        return super().model_post_init(context)
