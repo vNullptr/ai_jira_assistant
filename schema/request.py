04976bef7b23141bdc69d8a41889815a41503dab
vNullptr <ghstdu@gmail.com>
2026-07-21 14:53:07 +0100

    add: Field descriptions

--- schema/request.py
+++ schema/request.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel, Field
 from typing import List
+from langchain.tools import tool 
 
 class ExtractedMailData(BaseModel):
-    client : str | None
-    product : List[str]
-    rtype : str
-    priority : int
-    description : str
-    base_txt : str 
+    client : str | None = Field(description="Name of the client reporting.")
+    product : List[str] = Field(description="List of affected products.")
+    rtype : str = Field(description="Request type (Evolution/Incident/...)")
+    priority : int = Field(description="Priority/severity of the report.")
+    description : str = Field(description="Short description summarizing the initial report.")
+    base_txt : str = Field(description="Base report text.")
+    
