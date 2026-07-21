93587b28f47d332982a0acbec40618451f9f53f4
vNullptr <ghstdu@gmail.com>
2026-07-21 13:33:56 +0100

    add/refactor: added langfuse tracing and finished prompt() for MistralClient.

--- clients/llm.py
+++ clients/llm.py
@@ -1,30 +1,40 @@
 from pydantic import BaseModel, Field
+from typing import Optional, ClassVar
 from abc import ABC, abstractmethod
 from langchain_ollama import ChatOllama
 from langchain_core.prompts import BaseChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.language_models import BaseChatModel
+from langchain_core.messages.ai import AIMessage
+from langfuse import observe
 
-from schema.prompt import basePrompt
+from schema.prompt import extraction_prompt
+from config import Settings
 
 class LLMClient(BaseModel, ABC):
     model_name : str = Field()
+    # TODO : Useless here needs to be moved elsewhere
+    settings : ClassVar = Settings() 
     
     @abstractmethod
-    def prompt(self, prompt : BaseChatPromptTemplate, temperature : int = 0):
-        """Prompts the llm model.
+    def prompt(self, prompt_template : BaseChatPromptTemplate, prompt_content : dict = None) -> AIMessage:
+        """Prompts the model with a specific prompt template and content.
 
         Args:
-            prompt (PromptSchema): Prompt to be passed to the llm.
-            temperature (int, optional): Temperature of model. 
+            prompt_template (BaseChatPromptTemplate): The langchain prompt template
+            prompt_content (Optional, dict): Dictionary to inject inside the prompt template
+
+        Returns:
+            AIMessage : LLM answer.
         """
         pass
 
     
-    
 class MistralClient(LLMClient):
     """Client for ollama Mistral model"""
     
-    temperature : float = Field(gt=0, lt=2) # not on the abs because range differs from model to another
+    model_name : str = "mistral"
+    temperature : float = Field(ge=0, le=2) # not on the abs because range differs from model to another
+    client : Optional[BaseChatModel] = None
         
     def model_post_init(self, context: any) -> None: 
         self.client = ChatOllama(
@@ -32,9 +42,13 @@ class MistralClient(LLMClient):
             temperature=self.temperature,
             validate_model_on_init=True,
         )
+
+    @observe(name="Mistral Prompt")
+    def prompt(self, prompt_template : BaseChatPromptTemplate, prompt_content : dict = None) -> AIMessage:
         
-    # still deciding on prototype
-    def prompt_ctx(self, prompt_template : BaseChatPromptTemplate, prompt_content : str) -> any:
-        pass 
+        chain = (prompt_template
+            | self.client)
+        result = chain.invoke(prompt_content)
         
-         
+        return result 
+        
