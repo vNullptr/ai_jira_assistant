faf7c6627cb0514e74db3a391429f1e121eb33d5
vNullptr <ghstdu@gmail.com>
2026-07-24 17:29:27 +0100

    refactor/add: added get_issue() + comment_issue() + update_comment() + added Content-Type to header

--- clients/jira.py
+++ clients/jira.py
@@ -1,20 +1,47 @@
 from pydantic import BaseModel, PrivateAttr, Field, computed_field, field_validator
 from abc import ABC, abstractmethod
-import httpx, asyncio, json
+import httpx, json
 
 class JiraClient(BaseModel, ABC):
     """Jira Client used to communicate with a project."""
     
     @abstractmethod
-    def get_issue_comments(self, id: int) -> list:
-        """Fetchs comments from an issue by id.
+    async def get_issue(self, issue_key: str) -> list:
+        """Fetchs the issue thread by id.
 
         Args:
-            id (int): id of the jira issue.
+            issue_key (int): jira issue key.
             
         Returns:
-        Returns a list of json objects containing all the comments.
+        issue_thread (dict): dict containing all the thread.
         """
+        pass
+    
+    @abstractmethod
+    async def comment_issue(self, issue_key: str, content: str):
+        """Comments on a specific issue.
+
+        Args:
+            issue_key (str): jira issue key.
+            content (str): content of the comment.
+
+        Returns:
+            response (dict): HTTP reponse, None if wrong status code.
+        """
+        pass
+    
+    @abstractmethod
+    async def update_comment(self, issue_key: str, comment_id: str):
+        """Updates/Edits specific comment.
+        
+        Args:
+            issue_key (str): jira issue key.
+            comment_id (str): id of comment to update.
+            content (str): content of the comment.
+
+        Returns:
+            response (dict): HTTP reponse, None if wrong status code.
+        """        
         pass
 
 class JiraAPIClient(JiraClient):
@@ -30,7 +57,8 @@ class JiraAPIClient(JiraClient):
             timeout=30,
             auth=httpx.BasicAuth(self.auth_mail, self.api_token),
             headers={
-                'Accept': 'application/json'
+                'Accept': 'application/json',
+                "Content-Type": "application/json"
             }
         )
         return super().model_post_init(context)
@@ -45,8 +73,74 @@ class JiraAPIClient(JiraClient):
     def api_url(self) -> str:
         return f"https://{self.domain}/rest/api/3"
     
-    async def get_issue_comments(self, id: int) -> list:
-        response = await self._client.get(f"issue/{id}/comment")
+    async def get_issue(self, issue_key: str) -> list:
+        response = await self._client.get(f"issue/{issue_key}")
         # TODO: Handling wrong status code with tenacity retry
         if response.status_code == 200:
             return json.loads(response.content)
+        
+    async def comment_issue(self, issue_key: str, content: str):
+        payload = json.dumps({
+        "body": {
+            "content": [
+            {
+                "content": [
+                {
+                    "text": content,
+                    "type": "text"
+                }
+                ],
+                "type": "paragraph"
+            }
+            ],
+            "type": "doc",
+            "version": 1
+        },
+        "properties": [
+                {
+                    "key": "sd.public.comment",
+                    "value": {
+                        "internal": True
+                    }
+                }
+            ]
+        })
+        
+        response = await self._client.post(f"issue/{issue_key}/comment",data=payload)
+        
+        if response.status_code == 201:
+            return json.loads(response.content)
+        
+    
+    async def update_comment(self, issue_key: str, comment_id: str, content: str):
+        payload = json.dumps({
+            "body": {
+                "content": [
+                {
+                    "content": [
+                    {
+                        "text": content,
+                        "type": "text"
+                    }
+                    ],
+                    "type": "paragraph"
+                }
+                ],
+                "type": "doc",
+                "version": 1
+            },
+            "properties": [
+                {
+                    "key": "sd.public.comment",
+                    "value": {
+                        "internal": True
+                    }
+                }
+            ]
+        })
+        
+        response = await self._client.put(f"issue/{issue_key}/comment/{comment_id}",data=payload)
+        
+        if response.status_code == 200:
+            return json.loads(response)
+        
