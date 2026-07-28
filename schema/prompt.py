30d4493cbe0ee8a87955b4f8d7394aa137374410
vNullptr <ghstdu@gmail.com>
2026-07-28 12:21:51 +0100

    fix: deleted old useless files.

--- schema/prompt.py
+++
@@ -1,8 +0,0 @@
-from langchain_core.prompts import ChatPromptTemplate
-from langfuse import get_client
-from config import Settings
-
-settings = Settings()
-langfuse = get_client()
-
-extraction_prompt = ChatPromptTemplate(langfuse.get_prompt("mail-extraction-classification", label="production").get_langchain_prompt())
